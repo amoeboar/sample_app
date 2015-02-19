cf9b1c71dd91a42c5ec29752cdd0045e361c53e2
John Capogna <john.s.capogna@gmail.com>
2015-02-19 20:57:56 +0000

    added ccsf_rails controller with about, links and index views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'ccsf_rails/index'
+
+  get 'ccsf_rails/links'
+
+  get 'ccsf_rails/about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'application#hello'
+  root 'ccsf_rails#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
