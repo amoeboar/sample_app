8e4e1b0ce8ed8a87d96841a99d4b021a64a67d42
John Capogna <john.s.capogna@gmail.com>
2015-02-19 21:02:21 +0000

    adding controller route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def hello
-    render text: "hello, world!"
+  def index
   end
+
+  # def hello
+  #   render text: "hello, world!"
+  # end
   
 end
