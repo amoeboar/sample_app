504cd0cff064d144fb63e997d529f6c782132bbd
John Capogna <john.s.capogna@gmail.com>
2015-02-25 05:10:44 +0000

    Testing static pages

--- test/controllers/ccsf_rails_controller_test.rb
+++ test/controllers/ccsf_rails_controller_test.rb
@@ -4,16 +4,18 @@ class CcsfRailsControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select "title", "Index | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get links" do
     get :links
     assert_response :success
+    assert_select "title", "Links | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
-
 end
